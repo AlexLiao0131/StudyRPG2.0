8b5f7a35b886cb5e82810c8083e85d5155620c80
AlexLiao0131 <as315012@gmail.com>
2026-09-25 21:53:50 +0800

    Add files via upload

--- js/dungeon/dungeon-service.js
+++ js/dungeon/dungeon-service.js
@@ -1,3 +1,4 @@
-import { encounterForDate } from '../world/world-service.js';
+import { encounterForDate, worldForDate } from '../world/world-service.js';
 import { combatPower } from '../progression/combat-power.js';
-export function dungeonSnapshot(){const encounter=encounterForDate();return{encounter,heroPower:combatPower(),celebration:encounter===null}}
+import { calibratedDailyMonsterPower, ensureDailyBalanceSnapshot } from '../balance/daily-dungeon-power.js';
+export function dungeonSnapshot(){const encounter=encounterForDate(),world=worldForDate();if(!encounter)return{encounter:null,world,heroPower:combatPower(),enemyPower:0,celebration:!!world?.noBattle};const balance=ensureDailyBalanceSnapshot();return{encounter,world,heroPower:combatPower(),enemyPower:calibratedDailyMonsterPower(),balance,celebration:false}}
