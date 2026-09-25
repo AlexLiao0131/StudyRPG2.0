4149e60b0a1f72cee67ffd6a0f411f4fcc2101d8
AlexLiao0131 <as315012@gmail.com>
2026-09-25 20:59:54 +0800

    Add files via upload

--- js/dungeon/dungeon-service.js
+++ js/dungeon/dungeon-service.js
@@ -1,1 +1,3 @@
-
+import { encounterForDate } from '../world/world-service.js';
+import { combatPower } from '../progression/combat-power.js';
+export function dungeonSnapshot(){const encounter=encounterForDate();return{encounter,heroPower:combatPower(),celebration:encounter===null}}
