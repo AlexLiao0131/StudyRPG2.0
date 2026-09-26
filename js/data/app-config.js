17db83a05992f512ed61be47b8bda3fa5c4f61ed
AlexLiao0131 <as315012@gmail.com>
2026-09-26 09:52:52 +0800

    Add files via upload

--- js/data/app-config.js
+++ js/data/app-config.js
@@ -3,6 +3,7 @@ export const APP_CONFIG = Object.freeze({
   storageKey: 'StudyRPG2_STATE',
   legacyFamilyKey: 'heroRPG_FAMILY_V8',
   legacySingleKey: 'heroRPG_V2',
+  legacySeasonId: '2026_FALL',
   officialLaunchDate: '2026-08-31',
   assetBase: 'https://raw.githubusercontent.com/AlexLiao0131/studyRPG/main/'
 });
